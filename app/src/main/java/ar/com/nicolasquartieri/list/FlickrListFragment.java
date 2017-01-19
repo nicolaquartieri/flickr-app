77cb4e84f99915ff61f9860771f69a3139c48a40
Nicolas Quartieri <nquartieri@gmail.com>
2017-01-19 14:11:37 -0200

    UPDATE: Code format & EndlessRecyclerViewScrollListener added in R.V. listener list

--- app/src/main/java/ar/com/nicolasquartieri/list/FlickrListFragment.java
+++ app/src/main/java/ar/com/nicolasquartieri/list/FlickrListFragment.java
@@ -37,183 +37,216 @@ import ar.com.nicolasquartieri.widget.recyclerview.ItemOffsetDecoration;
 
 /**
  * Display the list of {@link Photo} collected from the {@link FlickrSearchApiService}.
+ *
  * @author Nicolas Quartieri (nicolas.quartieri@gmailn.com)
  */
 public class FlickrListFragment extends BaseFragment
-		implements LoaderManager.LoaderCallbacks<Cursor> {
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
-	/** Recycler View */
-	private RecyclerView mRecyclerView;
-	/** Recycler View Adapter */
-	private FlickrAdapter mAdapter;
-	/** Nothing Layout */
-	private LinearLayout mNothingLayout;
-	/** Amount of {@link Photo} columns in the list */
-	private static final int COLUMNS = 3;
-	/** Query search  */
-	private String mQuery;
-	/** Endless Recycler View Scroll Listener */
-	private EndlessRecyclerViewScrollListener mEndlessRecyclerViewScrollListener;
-	/** Current service */
-	private Intent mCurrentService;
+    /** Recycler View */
+    private RecyclerView mRecyclerView;
+    /** Recycler View Adapter */
+    private FlickrAdapter mAdapter;
+    /** Nothing Layout */
+    private LinearLayout mNothingLayout;
+    /** Amount of {@link Photo} columns in the list */
+    private static final int COLUMNS = 3;
+    /** Query search */
+    private String mQuery;
+    /** Current service */
+    private Intent mCurrentService;
+    /** Grid Layout Manager */
+    private GridLayoutManager mGridlayoutManager;
+    /** Linear Layout Manager */
+    private LinearLayoutManager mLinearLayoutManager;
+    /** Grid Endless Recycler View Scroll Listener */
+    private EndlessRecyclerViewScrollListener mGridEndlessRecyclerViewScrollListener;
+    /** Linear Endless Recycler View Scroll Listener */
+    private EndlessRecyclerViewScrollListener mLinearEndlessRecyclerViewScrollListener;
 
-	/**
-	 * New {@link FlickrListFragment} instance.
-	 * @return The fragment.
+    /**
+     * New {@link FlickrListFragment} instance.
+     *
+     * @return The fragment.
      */
-	public static FlickrListFragment newInstance() {
-		return new FlickrListFragment();
-	}
+    public static FlickrListFragment newInstance() {
+        return new FlickrListFragment();
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setHasOptionsMenu(true);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
-	@Override
-	public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
-			final Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_photo_list, container, false);
-	}
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+            final Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_photo_list, container, false);
+    }
 
-	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-		mNothingLayout = (LinearLayout) view.findViewById(R.id.nothing_layout);
-		mRecyclerView = (RecyclerView) view.findViewById(R.id.photo_list);
-		// Grid Layout Manager.
-		GridLayoutManager mGridlayoutManager = new GridLayoutManager(getActivity(), COLUMNS,
-				GridLayoutManager.VERTICAL, false);
-		mEndlessRecyclerViewScrollListener = new EndlessRecyclerViewScrollListener(mGridlayoutManager) {
-			@Override
-			public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-				if (mCurrentService.getStringExtra(ApiIntentService.EXTRA_API_SERVICE_ID)
-						.equalsIgnoreCase(FlickrSearchApiService.ID)) {
-					mCurrentService = FlickrSearchApiService.newIntent(getActivity(), mQuery, page + 1);
-				} else {
-					mCurrentService = FlickrLastestPhotoApiService.newIntent(getActivity(), page + 1);
-				}
-				getActivity().startService(mCurrentService);
-			}
-		};
-		mRecyclerView.addOnScrollListener(mEndlessRecyclerViewScrollListener);
-		mRecyclerView.setLayoutManager(mGridlayoutManager);
-		mRecyclerView
-				.addItemDecoration(new ItemOffsetDecoration(getActivity(), R.dimen.m0_125));
-		// Avoid open multiple wallpaper screen at the same time. just take the first one.
-		mRecyclerView.setMotionEventSplittingEnabled(false);
-	}
+        mNothingLayout = (LinearLayout) view.findViewById(R.id.nothing_layout);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.photo_list);
+        // Grid Layout Manager.
+        mGridlayoutManager = new GridLayoutManager(getActivity(), COLUMNS,
+                GridLayoutManager.VERTICAL, false);
+        // Linear Layout Manager.
+        mLinearLayoutManager = new LinearLayoutManager(getContext());
+        // EndlessRecyclerViewScrollListener for Grid Layout Manager.
+        mGridEndlessRecyclerViewScrollListener = new EndlessRecyclerViewScrollListener(mGridlayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                onLoadMoreElements(page);
+            }
+        };
+        // EndlessRecyclerViewScrollListener for Linear Layout Manager.
+        mLinearEndlessRecyclerViewScrollListener = new EndlessRecyclerViewScrollListener(mLinearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                onLoadMoreElements(page);
+            }
+        };
+        mRecyclerView.addOnScrollListener(mGridEndlessRecyclerViewScrollListener);
+        mRecyclerView.addOnScrollListener(mLinearEndlessRecyclerViewScrollListener);
+        mRecyclerView.setLayoutManager(mGridlayoutManager);
+        mRecyclerView
+                .addItemDecoration(new ItemOffsetDecoration(getActivity(), R.dimen.m0_125));
+        // Avoid open multiple wallpaper screen at the same time. just take the first one.
+        mRecyclerView.setMotionEventSplittingEnabled(false);
+    }
 
-	@Override
-	protected void onStartLoading() {
-		super.onStartLoading();
-		// Sync cause data from remote service.
-		mCurrentService = FlickrLastestPhotoApiService.newIntent(getActivity());
-		getActivity().startService(mCurrentService);
-	}
+    /**
+     * Get more {@link Photo} through the respective service.
+     * @param page The requested page to deliver.
+     */
+    private void onLoadMoreElements(int page) {
+        int mCurrentPage = page + 1;
+        if (mCurrentService.getStringExtra(ApiIntentService.EXTRA_API_SERVICE_ID)
+                .equalsIgnoreCase(FlickrSearchApiService.ID)) {
+            mCurrentService = FlickrSearchApiService.newIntent(getActivity(), mQuery, mCurrentPage);
+        } else {
+            mCurrentService = FlickrLastestPhotoApiService.newIntent(getActivity(), mCurrentPage);
+        }
+        getActivity().startService(mCurrentService);
+    }
 
-	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-		inflater.inflate(R.menu.menu_search, menu);
-		SearchManager searchManager = (SearchManager) getActivity().getSystemService(
-				Context.SEARCH_SERVICE);
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        // Sync cause data from remote service.
+        mCurrentService = FlickrLastestPhotoApiService.newIntent(getActivity());
+        getActivity().startService(mCurrentService);
+    }
 
-		MenuItem searchMenuItem = menu.findItem(R.id.search_item);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_search, menu);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(
+                Context.SEARCH_SERVICE);
 
-		final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
-		searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
-		searchView.setIconifiedByDefault(false);
-		searchView.setQueryHint(getResources().getString(R.string.search));
+        MenuItem searchMenuItem = menu.findItem(R.id.search_item);
 
-		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-			@Override
-			public boolean onQueryTextSubmit(String query) {
-				startLoading();
-				// Clean DB.
-				AppDatabase.cleanDB(getActivity().getContentResolver());
-				mEndlessRecyclerViewScrollListener.resetState();
-				// Call new Search.
-				mQuery = query;
-				mCurrentService = FlickrSearchApiService.newIntent(getActivity(), mQuery);
-				getActivity().startService(mCurrentService);
-				// Hide softkeyboard.
-				KeyboardUtils.hideSoftKeyboard(getActivity(), searchView);
-				return true;
-			}
-			@Override
-			public boolean onQueryTextChange(String s) {
-				return false;
-			}
-		});
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchMenuItem);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setQueryHint(getResources().getString(R.string.search));
 
-		super.onCreateOptionsMenu(menu, inflater);
-	}
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                startLoading();
+                // Clean DB.
+                AppDatabase.cleanDB(getActivity().getContentResolver());
+                mGridEndlessRecyclerViewScrollListener.resetState();
+                mLinearEndlessRecyclerViewScrollListener.resetState();
+                // Call new Search.
+                mQuery = query;
+                mCurrentService = FlickrSearchApiService.newIntent(getActivity(), mQuery);
+                getActivity().startService(mCurrentService);
+                // Hide softkeyboard.
+                KeyboardUtils.hideSoftKeyboard(getActivity(), searchView);
+                return true;
+            }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.settings_item:
-				// Switch between Grid & Linear Layout.
-				RecyclerView.LayoutManager layoutManager;
-				if (item.getIcon().getConstantState().equals(getResources().getDrawable(R.mipmap.ic_view_module).getConstantState())) {
-					item.setIcon(R.mipmap.ic_view_list);
-					layoutManager = new GridLayoutManager(getActivity(), COLUMNS,
-							GridLayoutManager.VERTICAL, false);
-				} else {
-					item.setIcon(R.mipmap.ic_view_module);
-					layoutManager = new LinearLayoutManager(getContext());
-				}
-				mRecyclerView.setLayoutManager(layoutManager);
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
-		}
-	}
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 
-	@Override
-	protected void onInitializeLoader(LoaderManager manager) {
-		super.onInitializeLoader(manager);
-		initLoader(manager, R.id.loader_photos_list, null, this);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings_item:
+                // Switch between Grid & Linear Layout.
+                RecyclerView.LayoutManager layoutManager;
+                // FIXME: User setTag & getTag instead getDrawable.
+                if (item.getIcon().getConstantState().equals(getResources()
+                        .getDrawable(R.mipmap.ic_view_module).getConstantState())) {
+                    // Grid Screen.
+                    item.setIcon(R.mipmap.ic_view_list);
+                    layoutManager = mGridlayoutManager;
+                } else {
+                    // Linear Screen.
+                    item.setIcon(R.mipmap.ic_view_module);
+                    layoutManager = mLinearLayoutManager;
+                }
+                mRecyclerView.setLayoutManager(layoutManager);
+//                layoutManager.scrollToPosition(0);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-	@Override
-	protected String onCreateLoadingResponseAction() {
-		return FlickrSearchApiService.RESPONSE_ACTION;
-	}
+    @Override
+    protected void onInitializeLoader(LoaderManager manager) {
+        super.onInitializeLoader(manager);
+        initLoader(manager, R.id.loader_photos_list, null, this);
+    }
 
-	@Override
-	protected void onCreateAdapter(Context context) {
-		super.onCreateAdapter(context);
-		if (mAdapter == null) {
-			mAdapter = new FlickrAdapter(this);
-		}
-		mRecyclerView.setAdapter(mAdapter);
-	}
+    @Override
+    protected String onCreateLoadingResponseAction() {
+        return FlickrSearchApiService.RESPONSE_ACTION;
+    }
 
-	@Override
-	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-		List<Photo> photos = PhotoTable.parseAllCursor(cursor);
-		if (photos != null && !photos.isEmpty()) {
-			mAdapter.addPhotos(photos);
-			mNothingLayout.setVisibility(View.GONE);
-			mRecyclerView.setVisibility(View.VISIBLE);
-		} else {
-			mNothingLayout.setVisibility(View.VISIBLE);
-			mRecyclerView.setVisibility(View.GONE);
-			mAdapter.setPhotos(photos);
-		}
-	}
+    @Override
+    protected void onCreateAdapter(Context context) {
+        super.onCreateAdapter(context);
+        if (mAdapter == null) {
+            mAdapter = new FlickrAdapter(this);
+        }
+        mRecyclerView.setAdapter(mAdapter);
+    }
 
-	@Override
-	public void onLoaderReset(Loader<Cursor> loader) {
-		mAdapter.setPhotos(new ArrayList<Photo>());
-	}
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        List<Photo> photos = PhotoTable.parseAllCursor(cursor);
+        if (photos != null && !photos.isEmpty()) {
+            mAdapter.addPhotos(photos);
+            mNothingLayout.setVisibility(View.GONE);
+            mRecyclerView.setVisibility(View.VISIBLE);
+        } else {
+            mNothingLayout.setVisibility(View.VISIBLE);
+            mRecyclerView.setVisibility(View.GONE);
+            mAdapter.setPhotos(photos);
+        }
+    }
 
-	@Override
-	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-		return new CursorLoader(getActivity(), PhotoTable.buildUri(), null, null, null,
-				PhotoTable.COLUMN_ID + " desc");
-	}
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.setPhotos(new ArrayList<Photo>());
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(getActivity(), PhotoTable.buildUri(), null, null, null,
+                PhotoTable.COLUMN_ID + " desc");
+    }
 }
